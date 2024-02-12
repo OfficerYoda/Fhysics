33d94e4ea4480c032f6e396a709a724cce41735e
OfficerYoda <officeryoda@gmail.com>
2024-02-12 22:13:39 +0100

    RectangleExtensions for contains(Vector2(Int))

---
+++ src/main/java/de/officeryoda/fhysics/extensions/RectangleExtension.kt
@@ -0,0 +1,25 @@
+package de.officeryoda.fhysics.extensions
+
+import de.officeryoda.fhysics.engine.Vector2
+import de.officeryoda.fhysics.engine.Vector2Int
+import java.awt.Rectangle
+
+/**
+ * Checks if the Rectangle contains the specified Vector2.
+ *
+ * @param vector2 The Vector2 to check for containment.
+ * @return True if the Rectangle contains the Vector2, false otherwise.
+ */
+fun Rectangle.contains(vector2: Vector2): Boolean {
+    return contains(vector2.x, vector2.y)
+}
+
+/**
+ * Checks if the Rectangle contains the specified Vector2Int.
+ *
+ * @param vector2Int The Vector2Int to check for containment.
+ * @return True if the Rectangle contains the Vector2Int, false otherwise.
+ */
+fun Rectangle.contains(vector2Int: Vector2Int): Boolean {
+    return contains(vector2Int.x, vector2Int.y)
+}
