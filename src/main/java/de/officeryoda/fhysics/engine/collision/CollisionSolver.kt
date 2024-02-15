94b41461f04e1bd162b9378dceb74040e85a446f
OfficerYoda <officeryoda@gmail.com>
2024-02-15 02:21:35 +0100

    fixed separateOverlappingObjects

--- src/main/java/de/officeryoda/fhysics/engine/collision/CollisionSolver.kt
+++ src/main/java/de/officeryoda/fhysics/engine/collision/CollisionSolver.kt
@@ -9,11 +9,18 @@ abstract class CollisionSolver {
     abstract fun solveCollision(info: CollisionInfo)
 
     protected fun separateOverlappingObjects(info: CollisionInfo) {
-        val moveAmount: Vector2 = info.overlap * 0.5 * info.normal
+        val moveAmount: Vector2 = 0.5 * info.overlap * info.normal
+
+        val objA: FhysicsObject = info.objA!!
+        val objB: FhysicsObject = info.objB!!
+
+        // other object needs to move double the distance
+        if(objA.static || objB.static)
+            moveAmount *= 2.0
 
         // Move circles apart along the collision normal
-        moveIfNotStatic(info.objA!!, -moveAmount)
-        moveIfNotStatic(info.objB!!, moveAmount)
+        moveIfNotStatic(objA, -moveAmount)
+        moveIfNotStatic(objB, moveAmount)
     }
 
     private fun moveIfNotStatic(obj: FhysicsObject, moveAmount: Vector2) {
